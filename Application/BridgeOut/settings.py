8243a5f5a580530daa6589de30084111181a4a3b
Evan Duffield <duffieldevan@gmail.com>
2023-09-10 18:55:45 -0600

    Containerized server w/ docker

--- Application/BridgeOut/settings.py
+++ Application/BridgeOut/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import env_variables as ve
-import apps.helloapp
+import os
+# import env_variables as ve
+# import apps.helloapp
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ve.secret_key
+# Use this vvv to get secret key from console
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "2odni2o3nf32fno3i2fi3p3p59re"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
