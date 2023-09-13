8d5159c149c334f576ca23c41c03f7b9e06371a9
Ryan Lunas <luna0500@bears.unco.edu>
2023-09-13 15:41:23 -0600

    Test Server Config

--- ServerTestApp/config/settings.py
+++ ServerTestApp/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%#*&favrb*hsz8laqxwy8xn51g&&^_mu@=whaqew(g@$v_*x#=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- ServerTestApp/config/urls.py
+++ ServerTestApp/config/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from djangotest.views import IndexView
 
 urlpatterns = [
-    path('', index.html)
+    path('', IndexView.as_view()),
     path('admin/', admin.site.urls),
 ]
