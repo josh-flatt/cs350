5daa6e850743c4da3a701f721413511c878afb27
Ryan Lunas <luna0500@bears.unco.edu>
2023-10-01 18:15:27 -0600

    user model tests (couldn't get functioning)

--- Application/apps/helloapp/tests.py
+++ Application/apps/helloapp/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
 
-# Create your tests here.
+from helloapp.models import AppUser
+
+class AppUserModelTestCase(TestCase):
+    def setUp(self):
+        # Create a sample user for testing
+        self.user = AppUser.objects.create(
+            userName='testuser',
+            firstName='John',
+            lastName='Doe',
+            email='test@example.com',
+            birthDate='1990-01-01',
+            location='Test Location',
+            profilePicture='/static/profileImages/robot.jpg',
+            isLoggedIn=False,
+        )
+
+    def test_user_creation(self):
+        """
+        Test that a user can be created with valid data.
+        """
+        self.assertEqual(AppUser.objects.count(), 1)
+        saved_user = AppUser.objects.first()
+        self.assertEqual(saved_user.userName, 'testuser')
+        self.assertEqual(saved_user.firstName, 'John')
+        self.assertEqual(saved_user.lastName, 'Doe')
+        self.assertEqual(saved_user.email, 'test@example.com')
+        self.assertEqual(saved_user.birthDate, '1990-01-01')
+        self.assertEqual(saved_user.location, 'Test Location')
+        self.assertEqual(saved_user.profilePicture, 'profile.jpg')
+        self.assertFalse(saved_user.isLoggedIn)
+
+    def test_user_str_representation(self):
+        """
+        Test the __str__ method of the user model.
+        """
+        self.assertEqual(str(self.user), 'testuser')
