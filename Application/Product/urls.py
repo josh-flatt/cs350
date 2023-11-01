9bed20fb3aae0b05d92c0ab51d73c8d1ff1fd8cc
KyleLongaker <79126650+KyleLongaker@users.noreply.github.com>
2023-10-31 20:59:05 -0600

    Delete Application/Product directory

--- Application/Product/urls.py
+++
@@ -1,8 +0,0 @@
-# urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # ... your other URL patterns ...
-    path('add-product/', views.add_product, name='add_product'),
-]
