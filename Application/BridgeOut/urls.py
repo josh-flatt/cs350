c262a76841ef5479181e0e52eacffe96fa10e3ad
KyleLongaker <79126650+KyleLongaker@users.noreply.github.com>
2023-10-31 22:49:13 -0600

    Update urls.py

--- Application/BridgeOut/urls.py
+++ Application/BridgeOut/urls.py
@@ -1,19 +1,24 @@
 """
-URL configuration for BridgeOut project.
+BridgeOut Project URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+This configuration provides a guide to mapping URLs to their respective views.
+For an in-depth understanding, refer to:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+Usage:
+- For function views:
+    1. Import your view:  from my_app import views
+    2. Add the URL to urlpatterns:  path('', views.home, name='home')
+    
+- For class-based views:
+    1. Import your view:  from other_app.views import Home
+    2. Add the URL to urlpatterns:  path('', Home.as_view(), name='home')
+    
+- To include another URL configuration:
+    1. Import the required functions: from django.urls import include, path
+    2. Include the URL configuration:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from .views_functions import homepage_view
@@ -23,7 +28,7 @@ urlpatterns = [
     path("", RedirectView.as_view(url="helloapp/")),
     path("admin/", admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/profile/',  RedirectView.as_view(url='/helloapp/user/home')),
+    path('accounts/profile/', RedirectView.as_view(url='/helloapp/user/home')),
     path('', homepage_view, name='homepage'),
     path("helloapp/", include("helloapp.urls")),
 ]
